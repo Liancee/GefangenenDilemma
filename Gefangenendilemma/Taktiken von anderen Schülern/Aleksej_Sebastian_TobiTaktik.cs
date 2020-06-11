e5c3ea50245a4d927e067f2416bb8b3dd3184f0b
ppreis <ppreis@arc-greenlab.de>
2020-06-11 19:49:22 +0200

    Taktiken push

---
+++ Gefangenendilemma/Taktiken von anderen Schülern/Aleksej_Sebastian_TobiTaktik.cs
@@ -0,0 +1,51 @@
+using Gefangenendilemma.Basis;
+using System;
+
+namespace Gefangenendilemma
+{
+    /// <summary>
+    /// Nur nutzen, wenn es ein 3. Gruppenmitglied gibt.
+    /// </summary>
+    public class Aleksej_Sebastian_TobiTaktik : BasisStrategie
+    {
+        private bool kopierer;
+        private bool reaktion;
+        Random random = new Random();
+        public override string Name()
+        {
+            return "Zufälliger Kopierer";
+        }
+
+        public override string Autor()
+        {
+            return "Aleksej Demtschuk";
+        }
+
+        public override void Start(int runde, int schwere)
+        {
+            reaktion = false;
+        }
+
+        public override int Verhoer(int letzteReaktion)
+        {
+            
+            kopierer = random.Next(0, 2) != 0;
+
+            if (letzteReaktion != Verrat)
+            {
+                reaktion = false;
+            }
+            else if (kopierer && letzteReaktion == Verrat)
+            {
+                reaktion = true;
+            }
+            else if (!kopierer && letzteReaktion == Verrat)
+            {
+                reaktion = false;
+            }
+
+            return reaktion ? Verrat : Kooperieren;
+
+        }
+    }
+}
