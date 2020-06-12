be06a26ebc798717ceb97bd0c044605a00818914
Paul Schueler <paulschler030@gmail.com>
2020-06-12 18:09:46 +0200

    Push meiner Taktik Periodisch und Unfreundlich // Paul Schüler

---
+++ Gefangenendilemma/Taktiken/periodisch und unfreundlich.cs
@@ -0,0 +1,33 @@
+﻿using Gefangenendilemma.Basis;
+using System;
+
+namespace Gefangenendilemma
+{
+    class periodisch_und_unfreundlich : BasisStrategie
+    {
+        private int Round { get; set; }
+        public override string Name()
+        {
+            return "periodisch und unfreundlich";
+        }
+        public override string Autor()
+        {
+            return "Paul Schüler";
+        }
+        public override void Start(int runde, int schwere)
+        {
+            this.Round = 0;
+        }
+        public override int Verhoer(int letzteReaktion)
+        {
+            Round++;
+            if (Round != 3)
+                return BasisStrategie.Verrat;
+            else
+            {
+                Round = 0;
+                return BasisStrategie.Kooperieren;
+            }
+        }
+    }
+}
